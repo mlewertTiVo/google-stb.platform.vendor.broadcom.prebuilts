bb56cfeed83b91f3b4ae8d4fc2c01237331831e8
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-01-08 13:28:18 -0800

    gms: update to gtvs-4497147 from 12.12.2017
    Bug:71711986
    Change-Id: If9870fcb8555203117bfc239eb02f8afe4aa7e45
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- gms/google/products/gms.mk
+++ gms/google/products/gms.mk
@@ -16,7 +16,6 @@
 
 PRODUCT_PACKAGES := \
     AndroidMediaShell \
-    AndroidPlatformServicesTV \
     AtvRemoteService \
     Backdrop \
     BugReportSender \
@@ -66,12 +65,14 @@ PRODUCT_COPY_FILES := \
     vendor/broadcom/prebuilts/gms/google/etc/permissions/privapp-permissions-google.xml:system/etc/permissions/privapp-permissions-google.xml \
     vendor/broadcom/prebuilts/gms/google/etc/permissions/privapp-permissions-atv.xml:system/etc/permissions/privapp-permissions-atv.xml
 
+
 # Overlay for GMS devices
 $(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/sample/products/location_overlay.mk)
+PRODUCT_PACKAGE_OVERLAYS += vendor/broadcom/prebuilts/gms/google/products/gms_tv_overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/broadcom/prebuilts/gms/google/products/gms_overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/broadcom/prebuilts/gms/google/products/overlay
+
 
 # Overrides
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.gmsversion=GTVS_0_Q3_2017
+    ro.com.google.gmsversion=O_release
